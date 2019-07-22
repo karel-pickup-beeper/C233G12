f02793d6e95d50a5401c20c78971322f35809815
Mark Pineda <markpineda160@gmail.com>
2019-07-22 14:31:11 -0600

    Updated JavaDoc.

--- KarelRPG/src/Collectible.java
+++ KarelRPG/src/Collectible.java
@@ -18,21 +18,51 @@ public class Collectible {
 		count=c.count;
 	}
 	
-	
+	/**
+	 * This accessor method will return a count, when called.
+	 * 
+	 * @Return count
+	 */
 	/* methods */
 	public int getCount() {
 		return count;
 	}
+	
+	/**
+	 * This mutator method will return an increased value of count, when called.
+	 * 
+	 * @Param c
+	 * @Return count
+	 */
 	public int increaseCount(int c) {
 		count+=c;
 		return count;	
 	}
+	/**
+	 * This accessor method will return the name of the collectible, when called.
+	 * 
+	 * @Return name
+	 */
 	public String getName() {
 		return name;
 	}
+	
+	/**
+	 * This mutator method will return string that contains the name of the collectible 
+	 * and the amount, when called.
+	 * 
+	 * @Return name
+	 * @Return count
+	 */
 	public String toString() {
 		return" (" + name+ ": " +count+ ")";
 	}
+	
+	/**
+	 * This  method will return true value if the player has successfully collected all items, and false if otherwise.
+	 * 
+	 * @Return isComplete
+	 */
 	public boolean isComplete(String name) {
 		if (Player.getSingleItemCount(name)>4) {
 			return true;
