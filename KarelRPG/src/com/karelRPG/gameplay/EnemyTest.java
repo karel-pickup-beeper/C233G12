c51ae3fb72fd0de74bc004ba189f6724bd5ffdd1
yasmin537 <52938483+yasmin537@users.noreply.github.com>
2019-08-11 18:51:58 -0600

    finished enemy test

--- KarelRPG/src/com/karelRPG/gameplay/EnemyTest.java
+++ KarelRPG/src/com/karelRPG/gameplay/EnemyTest.java
@@ -3,13 +3,20 @@ package com.karelRPG.gameplay;
 import static org.junit.Assert.*; 
 import org.junit.Test;
 
+import com.karelRPG.gameplay.Enemy.TileTakenException;
+
 public class EnemyTest { 
 	
 	public class EnemySub extends Enemy {
 		String type;
+		int attack;
 		
-		public EnemySub(int health, int x, int y, String t) {
+		public EnemySub(int health, int x, int y, String t, int a) {
 			super(health, x, y);
+			
+			if (a > 0 && a < 99 ) this.attack = a;
+			
+			super.setAttack(attack);
 			type = t;
 		}
 		
@@ -20,27 +27,23 @@ public class EnemyTest {
 		
 
 		@Override
-		protected String getType() {
+		public String getType() {
 			return this.type;
 		}
-
+	
 		@Override
-		public void enemyMove(int seen, boolean away, CardinalDirection there) {
-			
-		}
-
-		@Override
-		public void enemyAttack(Player target) {
-			
-		}
+		public void enemyAttack(Player target, Maps mapwalk, String direction) {
+						
+			if (target.getX()==this.getXloc()  &&  target.getY()==this.getYloc()) {
+				target.changeHealth(-attack);
+			}
 		
+		}
 	}
-	
-	
 	
 	@Test
 	public void test_Constructor() {
-		Enemy c = new EnemySub(7, 8, 6, "robot");
+		Enemy c = new EnemySub(7, 8, 6, "robot", 3);
 		
 		assertEquals("Testing Copy Constructor, copying initial health 7, xLoc 8, yLoc 6 and Enemy type"
 				+ " - testing initial health", 7, c.getHealth());
@@ -54,7 +57,7 @@ public class EnemyTest {
 	
 	@Test
 	public void test_CopyConstructor() {
-		Enemy c = new EnemySub(7, 8, 6, "zombie");
+		Enemy c = new EnemySub(7, 8, 6, "zombie", 3);
 		Enemy c2 = new EnemySub(c);
 		assertEquals("Testing Copy Constructor, copying initial health 7, xLoc 8, yLoc 6 and Enemy type"
 				+ " - testing initial health", 7, c2.getHealth());
@@ -66,91 +69,225 @@ public class EnemyTest {
 				+ " - testing title", "zombie", c2.getType());
 	}
 	
+	//testing the getters & setters
 	@Test
 	public void test_getHealth() {
-		Enemy e = new EnemySub(12, 5, 4, "zombie");
-		e.getHealth();
+		Enemy e = new EnemySub(12, 5, 4, "zombie", 3);
 	
-		assertEquals("Testing getHealth", 12, e.getHealth());
+		int expected = 12;
+		int actual = e.getHealth();
+	
+		assertEquals("Testing getHealth", expected, actual);
 	}
 	
 	@Test
 	public void test_getXloc() {
-		Enemy e = new EnemySub(12, 5, 4, "ghost");
-		e.getXloc();
-	
-		assertEquals("Testing getXloc", 5, e.getXloc());
+		Enemy e = new EnemySub(12, 5, 4, "ghost", 3);
+		
+		int expected = 5;
+		int actual = e.getXloc();
+			
+		assertEquals("Testing getXloc", expected, actual);
 	}
 	
 	@Test
 	public void test_getYloc() {
-		Enemy e = new EnemySub(12, 5, 4, "robot");
-		e.getYloc();
-	
-		assertEquals("Testing getYloc", 4, e.getYloc());
+		Enemy e = new EnemySub(12, 5, 4, "robot", 3);
+			
+		int expected = 4;
+		int actual = e.getYloc();
+		
+		assertEquals("Testing getYloc", expected, actual);
 	}
 	
 	@Test
 	public void test_getType() {
-		Enemy e = new EnemySub(12, 5, 4, "zombie");
+		Enemy e = new EnemySub(12, 5, 4, "zombie", 6);
 		e.getType();
 	
-		assertEquals("Testing getType", "zombie", e.getType());
+		String expected = "zombie";
+		String actual = e.getType();
+		
+		assertEquals("Testing getType", expected, actual);
 	}
 
+	//testing methods that change the instance variables
 	@Test
 	public void test_loseHealth() {
-		Enemy e = new EnemySub(12, 5, 4,"ghost");
+		Enemy e = new EnemySub(12, 5, 4,"ghost", 6);
 		e.getHealth();
 		e.loseHealth(4);
 	
-		assertEquals("Testing loseHealth", 8, e.getHealth());
+		int expected = 8;
+		int actual = e.getHealth();
+		
+		assertEquals("Testing loseHealth", expected, actual);
 	}
 
 	@Test
 	public void test_changeYloc() {
-		Enemy e = new EnemySub(12, 5, 4, "robot");
+		Enemy e = new EnemySub(12, 5, 4, "robot", 6);
 		e.getYloc();
 		e.changeYloc(7);
 	
-		assertEquals("Testing changeYloc", 11, e.getYloc());
+		int expected = 11;
+		int actual = e.getYloc();
+		
+		assertEquals("Testing changeYloc", expected, actual);
 	}
 	
 	@Test
 	public void test_changeXloc() {
-		Enemy e = new EnemySub(12, 5, 4, "ghost");
+		Enemy e = new EnemySub(12, 5, 4, "ghost", 6);
 		e.getXloc();
 		e.changeXloc(10);
+		
+		int expected = 15;
+		int actual = e.getXloc();
 	
-		assertEquals("Testing changeYloc", 15, e.getXloc());
+		assertEquals("Testing changeYloc", expected, actual);
 	}
 
+	//testing enemyAttack
+	@Test
+	public void test_enemyAttack() {
+		Enemy e = new EnemySub(12, 8, 6, "ghost", 4);
+		Player p = new Player(7, 8, 6);
+		Maps m = new Maps(2);
+		
+		e.enemyAttack(p, m, "NORTH");
+		e.getAttack();
+		
+		int expected = 3;
+		int actual = p.getHealth();
+		
+		assertEquals("Testing enemyAttack", expected, actual);
+	}
 	
 	@Test
-	public void test_toString_1() {
-		Enemy e = new EnemySub(12, 5, 4, "ghost");
-		e.getType();
-		e.toString();
+	public void test_enemyAttack_noAttack() {
+		Enemy e = new EnemySub(12, 5, 8, "ghost", 4);
+		Player p = new Player(7, 8, 6);
+		Maps m = new Maps(2);
+		
+		e.enemyAttack(p, m, "NORTH");
+		e.getAttack();
+		
+		int expected = 7;
+		int actual = p.getHealth();
+		
+		assertEquals("Testing enemyAttack", expected, actual);
+	}
 	
-		assertEquals("Testing toString", " {ghost(12)}", e.toString());
+	//testing enemyMove
+	@Test
+	public void test_enemyMove_EAST() {
+		Player p = new Player(4, 15, 8);
+		Enemy e = new EnemySub(12, 5, 8, "ghost", 4);
+		Maps m = new Maps(2);
+		
+		try {
+			e.enemyMove(CardinalDirection.EAST, p, m);
+		} catch (TileTakenException e1) {
+			e1.printStackTrace();
+		}
+		
+		int actual = e.getXloc();
+		int expected = 6;
+		assertEquals("Testing enemyMove: east", expected, actual);
+	}
+	
+	@Test
+	public void test_enemyMove_WEST() {
+		Player p = new Player(4, 15, 8);
+		Enemy e = new EnemySub(12, 5, 8, "ghost", 4);
+		Maps m = new Maps(2);
+		
+		try {
+			e.enemyMove(CardinalDirection.WEST, p, m);
+		} catch (TileTakenException e1) {
+			e1.printStackTrace();
+		}
+		
+		int actual = e.getXloc();
+		int expected = 4;
+		assertEquals("Testing enemyMove: west", expected, actual);
+	}
+	
+	@Test
+	public void test_enemyMove_NORTH() {
+		Player p = new Player(4, 15, 8);
+		Enemy e = new EnemySub(12, 5, 7, "ghost", 4);
+		Maps m = new Maps(2);
+		
+		try {
+			e.enemyMove(CardinalDirection.NORTH, p, m);
+		} catch (TileTakenException e1) {
+			e1.printStackTrace();
+		}
+		
+		int actual = e.getYloc();
+		int expected = 6;
+		assertEquals("Testing enemyMove: north", expected, actual);
+	}
+	
+	//Testing isSpaceClear for true and false outcomes
+	@Test
+	public void test_isSpaceClear_TRUE() {
+		Enemy e = new EnemySub(12, 5, 8, "ghost", 4);
+		Player p = new Player(4, 15, 8);
+		Maps m = new Maps(2);
+		
+		boolean actual = e.isSpaceClear(6, 4, p, m);
+		boolean expected = true;
+		
+		assertEquals("Testing isSpaceClear: true", expected, actual);
+	}
+	
+	@Test
+	public void test_isSpaceClear_FALSE() {
+		Enemy e = new EnemySub(12, 5, 7, "ghost", 4);
+		Player p = new Player(4, 11, 8);
+		Maps m = new Maps(2);
+		
+		boolean actual = e.isSpaceClear(6, 1, p, m);
+		boolean expected = false;
+		
+		assertEquals("Testing isSpaceClear: false", expected, actual);
+	}
+	
+	//testing toString with 3 different enemy types
+	@Test
+	public void test_toString_1() {
+		Enemy e = new EnemySub(12, 5, 4, "ghost", 6);
+		e.getType();
+		
+		String expected =" {ghost(12)}";
+		String actual = e.toString();
+		
+		assertEquals("Testing toString", expected, actual);
 	}
 	
 	@Test
 	public void test_toString_2() {
-		Enemy e = new EnemySub(12, 5, 4, "zombie");
+		Enemy e = new EnemySub(12, 5, 4, "zombie", 6);
 		e.getType();
-		e.toString();
+		
+		String expected = " {zombie(12)}";
+		String actual = e.toString();
 	
-		assertEquals("Testing toString", " {zombie(12)}", e.toString());
+		assertEquals("Testing toString", expected, actual);
 	}
 
 	@Test
 	public void test_toString_3() {
-		Enemy e = new EnemySub(12, 5, 4, "robot");
+		Enemy e = new EnemySub(12, 5, 4, "robot", 6);
 		e.getType();
-		e.toString();
+		
+		String expected = " {robot(12)}";
+		String actual = e.toString();
 	
-		assertEquals("Testing toString", " {robot(12)}", e.toString());
+		assertEquals("Testing toString", expected, actual);
 	}
 	
 }
